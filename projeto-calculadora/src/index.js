cf21568850b3b7002cf271811d67a87539cf790d
AllesonCoelho <alleson_henrique@hotmail.com>
2020-12-23 16:52:34 -0300

    Componente Calculator

--- projeto-calculadora/src/index.js
+++ projeto-calculadora/src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Calculator from './main/calculador'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>Calculator</h1>
+      <Calculator />
+
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
