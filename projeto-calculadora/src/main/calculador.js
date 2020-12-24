440f14358c4043701cd68f583b1336b81ad42f17
AllesonCoelho <alleson_henrique@hotmail.com>
2020-12-24 16:49:43 -0300

    Implementando Logica #02

--- projeto-calculadora/src/main/calculador.js
+++ projeto-calculadora/src/main/calculador.js
@@ -29,7 +29,33 @@ export default class Calculator extends Component{
         this.setState({...initialState})
     }
     setOperation(operation){
-        console.log(operation)
+        if(this.state.current === 0){
+            this.setState({operation, current : 1, clearDisplay:true })
+        }else{
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+                
+            } catch (error) {
+                values[0] = this.state.values[0]
+                
+            }
+            
+            values[1] = 0
+
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+
+            })
+        }
 
     }
 
@@ -51,6 +77,7 @@ export default class Calculator extends Component{
 
             values[i] = newValue
             this.setState({values})
+            console.log(values)
         }
     }
 
@@ -61,7 +88,7 @@ export default class Calculator extends Component{
                 <Display value={this.state.displayValue}/>
                 <Button label="AC" click={this.clearMemory} triple/>
                 <Button label="/" click={this.setOperation} operation/>
-                <Button label="7" click={this.addDigit} opera/>
+                <Button label="7" click={this.addDigit}/>
                 <Button label="8" click={this.addDigit}/>
                 <Button label="9" click={this.addDigit}/>
                 <Button label="*" click={this.setOperation} operation/>
